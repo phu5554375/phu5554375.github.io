5e4eae162fbff20390ec7b50515cd1dfc9b9a383
phu5554375 <phu5554375@gmail.com>
2020-02-20 15:58:20 +0700

    uppp

---
+++ assets/config.rb
@@ -0,0 +1,8 @@
+http_path 			= "/"
+css_dir 				= "css"
+sass_dir 				= "scss"
+cache 					= false
+output_style 		= :compact # :expanded :nested :compact :compressed
+line_comments 	= false
+sourcemap 			= true
+
