ed4e2edc4ef9ba347ad71def22adcc0f1c9e93fc
phu5554375 <phu5554375@gmail.com>
2020-07-13 21:03:39 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
- 
-  
+
+
     var owl = $('#owl-silder');
 owl.owlCarousel({
     items:1,
@@ -10,6 +10,7 @@ owl.owlCarousel({
     autoplayTimeout:2000,
     autoplayHoverPause:true,
     navigation : true,
+
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
@@ -18,35 +19,33 @@ $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
     $("#owl-logo").owlCarousel({
-   
+
         navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true,
-        items : 4, 
+        items : 4,
         itemsDesktop : false,
         itemsDesktopSmall : false,
         itemsTablet: false,
         itemsMobile : false,
         loop : true,
-   
+
     });
     $("#owl-feedback").owlCarousel({
-   
+
         navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true,
         loop : true,
-        items : 1, 
+        items : 1,
         nav:true,
         itemsDesktop : false,
         itemsDesktopSmall : false,
         itemsTablet: false,
         itemsMobile : false
-   
-    });
-   
-  });
 
-  
+    });
+
+  });
